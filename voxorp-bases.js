3e9b452bdeabe7d03c0c888640262ab0d5e14d51
KaaBEL <som.kaabel@gmail.com>
2022-11-13 20:27:18 +0100

    v0.0.1 - updgraded jsDoc comments - improved canvas refreshing - first testing place tile function

--- voxorp-bases.js
+++ voxorp-bases.js
@@ -1,21 +1,33 @@
 "use strict";
-// v0.0
+// v0.0.1
 
 var hiddenLogs = [],
-  /** @type {Array<string>} */ imgIDs = [],
-  /** @type {Array<HTMLImageElement>} */ images = [];
-
+/** @type {Array<string>} */
+  imgIDs = [],
+/** @type {Array<HTMLImageElement>} */
+  images = [];
 /**
- * @constructor
- * @param {VoxTile|array|string} tile
+ * @typedef {number} integer
+ */
+/**
+ * @param {VoxTile|array|string} tile / type
+ * @param {string} name
+ * @param {integer} x
+ * @param {integer} y
+ * @param {integer} rotation
  * @returns {VoxTile}
  */
 function VoxTile(tile) {
   if (tile instanceof VoxTile) {
+  /** @type {string} */
     this.type = tile.type;
+  /** @type {string} */
     this.name = tile.name;
+  /** @type {integer} */
     this.x = tile.x;
+  /** @type {integer} */
     this.y = tile.y;
+  /** @type {integer} */
     this.rotation = tile.rotation;
     Object.seal(this);
     return ;
@@ -32,7 +44,7 @@ function VoxTile(tile) {
     throw new TypeError("VoxTile costructor requires 5 arguments:" +
       " type, x, y, rotation");
   if (typeof a[1] !== "string")
-    throw new TypeError("1. argument \"name\" is not type of string");
+    throw new TypeError("2. argument \"name\" is not type of string");
   if (!Number.isInteger(a[2]))
     throw new TypeError("3. argument \"x\" is not an integer");
   if (!Number.isInteger(a[3]))
@@ -48,29 +60,81 @@ function VoxTile(tile) {
   Object.seal(this);
 }
 
-/**
+/** unsealed Typed array!
  * @constructor
  * @param {Array<object|Array<>>} arr
- * @ typedef {Array<VoxTile>} RenderedVoxObjects
- * @returns {RenderedVoxObjects}
+ * @returns {renderedVoxObjects}
  */
 function RenderedVoxObjects(arr) {
   Object.setPrototypeOf(this, Array.prototype);
   for (var i = this.length = arr.length; i-- > 0;)
+  /** @type {VoxTile} */
     this[i] = new VoxTile(arr[i]);
+  /** @type {function} attaches existing or creates new VoxTile */
   this.push = function push() {
     for (var i = arguments.length, args = [], a; i-- > 0;) {
       a = arguments[i];
       args[i] = a instanceof VoxTile ? a : new VoxTile(a);
     }
-    [].push.apply(this, args);
-    return this.length;
+    return [].push.apply(this, args);
+  }
+}
+/** position which tile has to be inserted in or replaced for
+ * @typedef {object} index
+ * @property {number} index.i
+ * @property {boolean} index.insert
+ */
+/** binary search from RenderedVoxObjects 
+ * @param {RenderedVoxObjects} where
+ * @param {number} x tile position
+ * @param {number} y tile position
+ * @returns {number} index to insert or replace tile
+*/
+function binaryFindPos(where, x, y) {
+  var bottom = 0, top = 0, middle;
+  while (1) {
+    middle = Math.floor((bottom + top) / 2 + 1);
+    if (bottom === top)
+      return {i: middle, insert: !0};
+    if (x > where[middle].x && y > where[middle].y)
+      bottom = middle;
+    else if (x === where[middle].x && y === where[middle].y)
+      return {i: middle, insert: !1};
+    else
+      top = middle - 1;
   }
 }
 
 var rendered = new RenderedVoxObjects([["building", "Wall", 0, 0, null], ["building", "Scorer", 0, 4, null]]);
 (rendered[1])
-// ! overwrites function init for intialization from previous scripts
+function placeRandomTile(x, y) {
+  var rand = Math.random() * 5 | 0, index = binaryFindPos(rendered, x, y);
+  var name = "Wall,Scorer,Dynamo empty,Crate,Laser tile".split(",")[rand];
+  var tile = new VoxTile("building", name, x, y, null);
+  if (index.insert) {
+    for (var i = index.i; i < rendered.length && rendered[i];)
+      i++;
+    if (i >= rendered.length)
+      rendered.push(rendered[--i]);
+    for (; i > index.i; i--)
+      rendered[i] = rendered[i - 1];
+  }
+  rendered[i] = tile;
+  render();
+}
+
+/*function placeRect(x0, y0, x1, y1) {
+  var i = 0, ;
+  while (x0 < x1)
+    ;
+}*/
+
+// ! overwrites function press for handling UIs from previous scripts
+function press(x, y) {
+  placeRandomTile(x, y);
+}
+
+// ! overwrites function render for rerendering from previous scripts
 function render() {
   var i = rendered.length, img, r = rendered;
   function imageByName(name) {
@@ -84,8 +148,8 @@ function render() {
     images.push(null);
     img.onload = function setImage() {
       images[this.id] = this;
+      render();
     };
-    setTimeout(render, 100);
   }
 
   ctx.reset();
@@ -94,24 +158,4 @@ function render() {
     if (r[i] && imageByName(r[i].name)) {
       ctx.drawImage(img, r[i].x * sc + vX, r[i].y * sc + vY, sc, sc);
     }
-}
-
-// ! overwrites function init for intialization from previous scripts
-function init() {
-  resizeWindow();
-}
-
-window.onresize = function () {
-  var  w = window.innerWidth, h = window.innerHeight, ratio;
-  if (w > 4096 || h > 4096)
-    if (w > h) {
-      h = h * 4096 / w;
-      w = 4096;
-    } else {
-      w = w * 4096 / h;
-      h = 4096;
-    }
-  canvas.width = w;
-  canvas.height = h;
-  render();
 }
