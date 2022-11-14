d95d067710e0695b48e3621c1fa4aecae8a00cf9
KaaBEL <som.kaabel@gmail.com>
2022-11-14 18:06:58 +0100

    v0.0.2 - tile placement is correct now - placed tiles are sorted by x then y

--- voxorp-bases.js
+++ voxorp-bases.js
@@ -1,5 +1,5 @@
 "use strict";
-// v0.0.1
+// v0.0.2
 
 var hiddenLogs = [],
 /** @type {Array<string>} */
@@ -78,6 +78,11 @@ function RenderedVoxObjects(arr) {
     }
     return [].push.apply(this, args);
   }
+  this.toString = function toString() {
+    for (var i = 0, s = ""; i < this.length; i++)
+      s += this[i].name + "," + this[i].x + "," + this[i].y + "; ";
+    return s;
+  }
 }
 /** position which tile has to be inserted in or replaced for
  * @typedef {object} index
@@ -88,37 +93,45 @@ function RenderedVoxObjects(arr) {
  * @param {RenderedVoxObjects} where
  * @param {number} x tile position
  * @param {number} y tile position
- * @returns {number} index to insert or replace tile
+ * @returns {index} index to insert or replace tile
 */
 function binaryFindPos(where, x, y) {
-  var bottom = 0, top = 0, middle;
+  var bottom = 0, top = where.length - 1, middle;
   while (1) {
-    middle = Math.floor((bottom + top) / 2 + 1);
     if (bottom === top)
-      return {i: middle, insert: !0};
-    if (x > where[middle].x && y > where[middle].y)
+      break;
+    middle = Math.floor((bottom + top + 1) / 2);
+    if (x > where[middle].x || x === where[middle].x && y > where[middle].y)
       bottom = middle;
-    else if (x === where[middle].x && y === where[middle].y)
+    else if (y === where[middle].y && x === where[middle].x)
       return {i: middle, insert: !1};
     else
       top = middle - 1;
   }
+  middle = top + (x > where[top].x ||
+    x === where[top].x && y > where[top].y);
+  if (middle === where.length)
+    return {i: middle, insert: !1};
+  return {i: middle, insert: (y !== where[top].y || x !== where[top].x)};
 }
 
-var rendered = new RenderedVoxObjects([["building", "Wall", 0, 0, null], ["building", "Scorer", 0, 4, null]]);
+var rendered = new RenderedVoxObjects([["building", "Wall", 0, 0, null]]);
 (rendered[1])
 function placeRandomTile(x, y) {
+  x = Math.floor((x - vX) / sc);
+  y = Math.floor((y - vY) / sc);
   var rand = Math.random() * 5 | 0, index = binaryFindPos(rendered, x, y);
   var name = "Wall,Scorer,Dynamo empty,Crate,Laser tile".split(",")[rand];
   var tile = new VoxTile("building", name, x, y, null);
-  if (index.insert) {
-    for (var i = index.i; i < rendered.length && rendered[i];)
+  var i = index.i;
+  if (index.insert)
+    while (i < rendered.length && rendered[i])
       i++;
-    if (i >= rendered.length)
-      rendered.push(rendered[--i]);
+  if (i >= rendered.length)
+    rendered.length++;
+  if (index.insert)
     for (; i > index.i; i--)
       rendered[i] = rendered[i - 1];
-  }
   rendered[i] = tile;
   render();
 }
@@ -152,7 +165,7 @@ function render() {
     };
   }
 
-  ctx.reset();
+  canvas.width = canvas.width;
   while (i-- > 0)
   // unloaded images returns null, undefined image returns undefined
     if (r[i] && imageByName(r[i].name)) {
